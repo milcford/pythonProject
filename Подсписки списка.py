48668b81dc04ef14c953694ccdbdaf466d90e171
milcford <milcford@gmail.com>
2023-09-09 12:46:42 +0300

    one commit

---
+++ Подсписки списка.py
@@ -0,0 +1,32 @@
+s = input().split()
+print(len(s))
+
+for i in range(len(s) + 1):
+    l1 = []
+    for i in range(len(s) - i):
+        l1.append([s[i]])
+    print(l1)
+
+    # l2 = []
+    for i in range(len(s) - 1):
+        l1.append([s[i],s[i + 1]])
+    print(l1)
+
+    # l3 = []
+    for i in range(len(s) - 2):
+        l1.append([s[i],s[i + 1],s[i + 2]])
+    print(l1)
+
+    # l4 = []
+    for i in range(len(s) - 3):
+        l1.append([s[i],s[i + 1],s[i + 2],s[i + 3]])
+    print(l1)
+
+    # l5 = []
+    for i in range(len(s) - 4):
+        l1.append([s[i],s[i + 1],s[i + 2],s[i + 3],s[i + 4]])
+    print(l1)
+
+
+
+
