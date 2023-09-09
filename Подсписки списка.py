1b0b9676fd936ee13ff1a6ba7efe0553bb46def1
milcford <milcford@gmail.com>
2023-09-09 15:07:29 +0300

    2

--- Подсписки списка.py
+++ Подсписки списка.py
@@ -1,31 +1,27 @@
 s = input().split()
+print(s)
 print(len(s))
 
 l1 = []
+
 for i in range(len(s) - 0):
-    l1.append([s[i + 0]])
+    l1.append(s[i:i + 1])
 print(l1)
 
-# l2 = []
 for i in range(len(s) - 1):
-    l1.append([s[i],s[i + 1]])
+    l1.append(s[i:i + 2])
 print(l1)
 
-# l3 = []
+
 for i in range(len(s) - 2):
-    l1.append([s[i],s[i + 1],s[i + 2]])
+    l1.append(s[i:i + 3])
 print(l1)
 
-# l4 = []
+
 for i in range(len(s) - 3):
-    l1.append([s[i],s[i + 1],s[i + 2],s[i + 3]])
+    l1.append(s[i:i + 4])
 print(l1)
 
-# l5 = []
 for i in range(len(s) - 4):
-    l1.append([s[i],s[i + 1],s[i + 2],s[i + 3],s[i + 4]])
+    l1.append(s[i:i + 5])
 print(l1)
-
-
-
-
